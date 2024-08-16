64f390aa3a3ab61b2e78efecf944110289fdf19e
Browser <92232900+browse999@users.noreply.github.com>
2024-08-16 20:15:19 +0300

    Add files via upload

---
+++ zamanbulma.js
@@ -0,0 +1,22 @@
+let  zaman
+
+zaman = Number(prompt(`Saat şuan kaç?`))  
+
+
+
+if (zaman<10){
+
+    alert('Sabah')
+}
+
+else if (zaman<19) {
+
+    alert('Öğlen')
+}
+
+else {
+
+    alert('Aksam')
+}
+
+
